c9dc2ebf2d9b16922259c704498deb8a790d1235
pointThink <69512713+pointThink@users.noreply.github.com>
2024-05-01 16:05:52 +0200

    Choose set by arg

--- src/Program.cs
+++ src/Program.cs
@@ -5,8 +5,7 @@ namespace Wallswitch;
 static class Program
 {
     static void Main(string[] args)
-    {
-        
+    { 
         Dictionary<string, WallpaperSet> sets = Config.GetSetsFromConfig();
         WallpaperSet set = sets[args[0]];
 
